9e9e85011b6449a06a1c008389c7f5799c78c5ce
wael kasouha <waelkas99@gmail.com>
2024-04-01 14:56:01 -0400

    update comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -20,6 +20,11 @@ class Comment extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
     // to check if comment is Approved
     public function isApproved()
     {
