d4790bff2fbf78d808b72f417f2ff14e172ed53f
wael kasouha <waelkas99@gmail.com>
2024-04-01 15:03:19 -0400

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,4 +49,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function review_ratings()
+    {
+        return $this->hasMany(ReviewRating::class);
+    }
 }
