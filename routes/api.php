3dfc602e41942837cc2a8505275e7e867b8683f9
Reham <reham.shaban2002@gmail.com>
2024-04-02 23:34:34 +0300

    User CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\PasswordController;
+use App\Http\Controllers\Api\Auth\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('auth/register', [AuthController::class, 'register']);
-
 Route::post('auth/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/users/{user}', [UserController::class, 'show']);
+    Route::put('/users/{user}', [UserController::class, 'update']);
+    Route::delete('/users/{user}', [UserController::class, 'destroy']);
+    Route::post('/change-password', [PasswordController::class, 'changeUserPassword']);
+});
